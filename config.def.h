d5582c55973d935253694cbf6d98cb9f7e681dc3
caduz <sl4v3r@gmail.com>
2021-03-05 01:12:52 +0000

    Some changes here and there

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -30,7 +30,7 @@ static const char *brightdown[]     = { "/usr/bin/brightnessctl", "s", "10%-", N
 
 /* tagging */
 /* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
+static const char *tags[] = { "main", "www", "dev", "news", "chat", "mail" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -40,8 +40,7 @@ static const Rule rules[] = {
 	/* class            instance    title       tags mask     isfloating   monitor */
 	{ "Firefox",        NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "Subl",           NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "Signal",         NULL,       NULL,       1 << 3,       0,           -1 },
-    { "thunderbird",    NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "Signal",         NULL,       NULL,       1 << 4,       0,           -1 },
 };
 
 /* layout(s) */
@@ -74,7 +73,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu-apps", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xfce4-terminal", NULL };
 static const char *thunarcmd[] = { "thunar", NULL };
-static const char *screenshotcmd[] = { "xfce4-screenshooter", NULL };
+static const char *screenshotcmd[] = { "flameshot-gui", NULL };
 
 /* navigate through tabs include */
 #include "shiftview.c"
